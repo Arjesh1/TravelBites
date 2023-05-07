52cc99944db509e1ead63b89cd0324150650d74a
Arjesh Khadka <109441944+Arjesh1@users.noreply.github.com>
2023-05-07 21:54:13 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// registration details
+let form = document.getElementById("regForm")
 
+regForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const fName = "fName:"+document.getElementById("fName").value
+    const lName = "lName:"+document.getElementById("lName").value
+    const phone = "phone:"+document.getElementById("phone").value
+    const email = "email:"+document.getElementById("email").value
+    const password = "password:"+document.getElementById("password").value
+console.log(fName);
+console.log(lName);
+console.log(phone);
+console.log(email);
+console.log(password);
+
+
+
+})
